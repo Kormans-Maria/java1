e031a234a7378d8016f654c4c97c07209c8a5c83
Kormans-Maria <maria.it@icloud.com>
2020-06-26 17:25:41 +0300

    3.14f AccuracyDoubleFloat

--- src/ru/progwards/java1/lessons/compare_if_cycles/TriangleSimpleInfo.java
+++ src/ru/progwards/java1/lessons/compare_if_cycles/TriangleSimpleInfo.java
@@ -2,20 +2,20 @@ package ru.progwards.java1.lessons.compare_if_cycles;
 
 public class TriangleSimpleInfo {
     public static int maxSide(int a, int b, int c){
-        if ((a > b) && (a > c))
+        if ((a >= b) && (a >= c))
             return a;
         else
-        if ((b > a) && (b > c))
+        if ((b >= a) && (b >= c))
             return b;
         else
             return c;
     }
 
     public static int minSide(int a, int b, int c){
-        if ((a < b) && (a < c))
+        if ((a <= b) && (a <= c))
             return a;
         else
-            if ((b < a) && (b < c))
+            if ((b <= a) && (b <= c))
                 return b;
             else
                 return c;
